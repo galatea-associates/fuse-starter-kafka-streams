e74a4b261e6adc21e79bdfae066c3cd95c308a5f
Grant Wade <wade@galatea-associates.com>
2020-05-25 15:39:58 -0400

    Issue-16 add prefix for kafka streams config that matches core dependency

--- core-kafka-streams/src/main/java/org/galatea/kafka/starter/messaging/KafkaStreamsConfig.java
+++ core-kafka-streams/src/main/java/org/galatea/kafka/starter/messaging/KafkaStreamsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties
+@ConfigurationProperties(prefix = "core-kafka-streams")
 public class KafkaStreamsConfig {
 
   @Getter
