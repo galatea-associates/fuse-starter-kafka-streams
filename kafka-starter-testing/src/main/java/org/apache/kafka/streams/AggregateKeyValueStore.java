96e9fdb1ac5a6e503189b439d958a1ae5f3525a4
Grant Wade <wade@galatea-associates.com>
2021-09-25 17:22:04 -0400

    AggregateKeyValueStore implement ReadOnlyKeyValueStore#all

--- kafka-starter-testing/src/main/java/org/apache/kafka/streams/AggregateKeyValueStore.java
+++ kafka-starter-testing/src/main/java/org/apache/kafka/streams/AggregateKeyValueStore.java
@@ -3,11 +3,18 @@ package org.apache.kafka.streams;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.processor.StateStore;
 import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 
 @RequiredArgsConstructor
 public class AggregateKeyValueStore<K, V> implements KeyValueStore<K, V> {
@@ -56,7 +63,57 @@ public class AggregateKeyValueStore<K, V> implements KeyValueStore<K, V> {
 
   @Override
   public KeyValueIterator<K, V> all() {
-    throw new UnsupportedOperationException();
+
+    Queue<KeyValueIterator<K, V>> populatedIterators = stores.stream()
+        .map(ReadOnlyKeyValueStore::all)
+        .filter(kvKeyValueIterator -> {
+          boolean hasNext = kvKeyValueIterator.hasNext();
+          if (!hasNext) {
+            kvKeyValueIterator.close();
+          }
+          return hasNext;
+        })
+        .collect(Collectors.toCollection(LinkedBlockingQueue::new));
+    AtomicReference<KeyValueIterator<K, V>> currentIter = new AtomicReference<>(null);
+    currentIter.set(populatedIterators.poll());
+
+    Supplier<Optional<KeyValueIterator<K,V>>> currentIterOpt = () -> {
+      while (currentIter.get() != null && !currentIter.get().hasNext()) {
+        currentIter.get().close();
+        currentIter.set(populatedIterators.poll());
+      }
+      return Optional.ofNullable(currentIter.get());
+    };
+
+    return new KeyValueIterator<K, V>() {
+      @Override
+      public void close() {
+        Optional.ofNullable(currentIter.get()).ifPresent(KeyValueIterator::close);
+        populatedIterators.forEach(KeyValueIterator::close);
+        populatedIterators.clear();
+      }
+
+      @Override
+      public K peekNextKey() {
+        return currentIterOpt.get()
+            .map(KeyValueIterator::peekNextKey)
+            .orElse(null);
+      }
+
+      @Override
+      public boolean hasNext() {
+        return currentIterOpt.get()
+            .map(KeyValueIterator::hasNext)
+            .orElse(false);
+      }
+
+      @Override
+      public KeyValue<K, V> next() {
+        return currentIterOpt.get()
+            .map(KeyValueIterator::next)
+            .orElse(null);
+      }
+    };
   }
 
   @Override
