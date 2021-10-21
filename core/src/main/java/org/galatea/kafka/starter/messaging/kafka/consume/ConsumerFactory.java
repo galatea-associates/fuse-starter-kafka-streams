2cdd0a69680dec25fed96b18e780ec4eebb9d683
Grant Wade <wade@galatea-associates.com>
2021-10-21 11:27:03 -0400

    Create ConsumerThread

---
+++ core/src/main/java/org/galatea/kafka/starter/messaging/kafka/consume/ConsumerFactory.java
@@ -0,0 +1,14 @@
+package org.galatea.kafka.starter.messaging.kafka.consume;
+
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.Deserializer;
+
+public interface ConsumerFactory {
+  <K, V> Consumer<K, V> create(Properties consumerProps, Deserializer<K> keySerde, Deserializer<V> valueSerde);
+
+  static ConsumerFactory defaultFactory() {
+    return KafkaConsumer::new;
+  }
+}
